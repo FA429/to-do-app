f511e922ac74e3de144f06ab0a45f8c5a18c04e3
faisal al-anezi <fa.429@hotmail.com>
2024-10-27 14:48:00 +0300

    deploy

--- src/hooks/use-toast.ts
+++ src/hooks/use-toast.ts
@@ -1,12 +1,8 @@
 "use client"
 
-// Inspired by react-hot-toast library
+import { ToastProps  } from "@radix-ui/react-toast"
 import * as React from "react"
-
-import type {
-  ToastActionElement,
-  ToastProps,
-} from "@/components/ui/toast"
+import { ToastActionElement } from "../components/ui/toast"
 
 const TOAST_LIMIT = 1
 const TOAST_REMOVE_DELAY = 1000000
@@ -18,6 +14,7 @@ type ToasterToast = ToastProps & {
   action?: ToastActionElement
 }
 
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 const actionTypes = {
   ADD_TOAST: "ADD_TOAST",
   UPDATE_TOAST: "UPDATE_TOAST",
@@ -93,8 +90,6 @@ export const reducer = (state: State, action: Action): State => {
     case "DISMISS_TOAST": {
       const { toastId } = action
 
-      // ! Side effects ! - This could be extracted into a dismissToast() action,
-      // but I'll keep it here for simplicity
       if (toastId) {
         addToRemoveQueue(toastId)
       } else {
